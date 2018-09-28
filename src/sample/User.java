2837e97c5a5d8773e232608f56921916d57c0265
Daemon-Macklin <daemon.macklin@outlook.com>
2018-09-28 12:01:24 +0100

    Adding files

---
+++ src/sample/User.java
@@ -0,0 +1,39 @@
+/*
+ * @Author Daemon Macklin
+ * Class for User Object
+ */
+
+package sample;
+
+import java.util.ArrayList;
+
+public class User {
+    private String name;
+    private String password;
+    private ArrayList<Activity> ToDoList;
+
+    public User(String name, String password){
+        this.setName(name);
+        this.setPassword(password);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ArrayList<Activity> getToDoList() {
+        return ToDoList;
+    }
+}
